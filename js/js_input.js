eb05dcbccf4d1fabd562d6476ce61a2642292ff6
Leandro Ricardo Guimarães <84518335+lr-guimaraes@users.noreply.github.com>
2021-11-01 11:19:31 -0300

    Add files via upload

---
+++ js/js_input.js
@@ -0,0 +1,22 @@
+function input(){
+
+	var keyword = ["SHERLOCK","MARVEL","+18"];
+    var card = ["https://lr-guimaraes.github.io/site-snopse/cards.html","https://lr-guimaraes.github.io/site-snopse/cards_marvel.html","https://github.com/lr-guimaraes/site-snopse/settings/pages"];
+    var keyword_input = document.getElementById("keyword").value;
+    var cont = 0;
+    
+    if(keyword.length =="" || keyword.length == null){
+        alert("Campo vazio, digite a categoria");
+    }
+   
+    for (var i = 0; i < keyword.length; i++){
+        if ( keyword_input.toUpperCase() == keyword[i]){
+            window.location.href = card[i];
+        }else{
+            cont += 1;
+        }
+    }
+    if (cont== keyword.length){
+        alert("Não encontramos series, filmes e livros com esse nome");
+    }
+}
