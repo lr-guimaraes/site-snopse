a9a7326c1d9b82398c72172e71a50621354fb34c
Leandro Ricardo Guimarães <84518335+lr-guimaraes@users.noreply.github.com>
2021-11-11 19:58:03 -0300

    Delete php directory

--- php/feedback.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $text = $_POST["txt"];
-
-    $con = mysqli_connect("localhost: 3306", "root", "root", "bank_tde" );
-
-    $result = mysqli_query($con, "INSERT INTO feedback (txt) VALUES('$text')");
-
-?>
