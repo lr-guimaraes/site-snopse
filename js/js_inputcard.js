48d5b96ae34384ccea51600fb43bd7f22901d5ef
Leandro Ricardo Guimarães <84518335+lr-guimaraes@users.noreply.github.com>
2021-11-12 13:04:17 -0300

    Update js_inputcard.js

--- js/js_inputcard.js
+++ js/js_inputcard.js
@@ -1,6 +1,6 @@
 function input(){
 
-	var keyword = ["SHERLOCK","MARVEL","+18"];
+	var keyword = ["SHERLOCK","MARVEL","classificação alta"];
     var card = ["cards.html","cards_marvel.html","card+18.html"];
     var keyword_input = document.getElementById("keyword").value;
     var cont = 0;
